a3c9634a522d581ddb66a2e19a362870d69e812f
Sajid Anam Ifti <ifti@nextgenitltd.com>
2024-10-31 00:26:14 +0600

    Deleted PDF After Download

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -12,21 +12,19 @@ class PDFController extends Controller
         if ($request->has('url')) {
 
             $url = $request->url;
-            $fileName = 'app/public/' . time() . '.pdf';
+            $fileName = 'app/public/PDF_' . time() . '.pdf';
             $delay = $request->delay ?? 5000;
 
             Browsershot::url($url)
-                ->setChromePath('/home/ifti/.cache/puppeteer/chrome/linux-130.0.6723.69/chrome-linux64/chrome')
                 ->format('A4')
                 ->margins(0, 0, 0, 0)
                 ->showBackground()
                 ->delay($delay)
                 ->savePdf(storage_path($fileName));
 
-                return response()->download(storage_path($fileName));
+                return response()->download(storage_path($fileName))->deleteFileAfterSend(true);
         }
 
-        // return json response
         return response()->json([
             'message' => 'No URL provided',
         ], 400);
